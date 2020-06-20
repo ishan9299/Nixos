cde62766e4b89208f92b7d528f709236a8f6d700
ishan <ishan365.ia@gmail.com>
2020-06-20 11:11:34 +0530

    some changes

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
   networking.useDHCP = false;
   networking.interfaces.ens3.useDHCP = true;
   networking.networkmanager.enable = true;
-  networking.interfaces.wlp2s0.useDHCP = true;
+  # networking.interfaces.wlp2s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -43,7 +43,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget git exa fish
+    wget git exa fish neovim flatpak
+    firefox
+  ];
+
+  environment.gnome3.excludePackages = with pkgs; [
+    gnome3.totem gnome3.epiphany gnome3.gnome-music
+    gnome-photos gnome3.yelp gnome3.gnome-contacts
   ];
 
   programs.fish.enable = true;
@@ -86,8 +92,10 @@
   # Enable the KDE Desktop Environment.
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome3.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.lightdm.greeter.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
 
   # NVIDIA stuff
   # hardware.nvidiaOptimus.disable = true;
