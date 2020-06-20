7861a13e84d6a5efc16089e5b0ba931c317e5d06
ishan <ishan365.ia@gmail.com>
2020-06-20 11:32:01 +0530

    some changes

--- configuration.nix
+++ configuration.nix
@@ -95,7 +95,7 @@
   # services.xserver.displayManager.sddm.enable = true;
   # services.xserver.desktopManager.plasma5.enable = true;
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.lightdm.greeter.enable = true;
+  services.xserver.displayManager.lightdm.greeter.enable = false;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome3.enable = true;
 
