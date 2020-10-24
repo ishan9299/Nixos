db6b6b09f5ef7f96308dc2349d89b4d1591598cf
ishan <ishan365.ia@gmail.com>
2020-10-24 16:43:44 +0530

    "enable the ca-references as an experimental-features"
    without this `nix profile install nixpkgs#{any_package_name}` fails.

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
   nix = {
    package = pkgs.nixUnstable;
    extraOptions = ''
-     experimental-features = nix-command flakes
+     experimental-features = nix-command flakes ca-references
    '';
   };
 
