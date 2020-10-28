cd0431d439c0ea8f6f23f67e87c26eb3e7ed4225
ishan <ishan365.ia@gmail.com>
2020-10-28 06:33:50 +0530

    some changes

--- flake.nix
+++ flake.nix
@@ -16,25 +16,73 @@
   };
 
   outputs = { self, unstable, home-manager, neovim, ... }@inputs:
+    let
+      inherit (builtins) attrNames attrValues readDir listToAttrs filter;
+      inherit (unstable) lib;
+      inherit (lib) removeSuffix recursiveUpdate genAttrs filterAttrs;
+
+      pkgImport = pkgs:
+        import pkgs {
+          inherit system;
+          overlays = attrValues self.overlays;
+          config = { allowUnfree = true; };
+        };
+
+      pkgset = {
+        pkgs = pkgImport unstable;
+      };
+
+      system = "x86_64-linux";
+    in
+    with pkgset;
   {
     nixos = self.nixosConfigurations.nixos.config.system.build.toplevel;
 
-    overlays = {
-      neovim = import ./overlays/neovim.nix;
-    };
+    # Automatically source the overlays directory
+    # got this from https://github.com/bqv/nixrc/blob/live/flake.nix#L538
+    overlays = listToAttrs (map (name: {
+      name = lib.removeSuffix ".nix" name;
+      value = import (./overlays + "/${name}");
+    }) (filter (file: lib.hasSuffix ".nix" file) (attrNames (readDir ./overlays))));
+
+    # Not Sure why is this required but the
+    # template for nrdxp seems to do this
+    # https://github.com/nrdxp/nixflk/blob/template/flake.nix#L57
+    packages."${system}" =
+      let
+        overlays = lib.filterAttrs (n: v: n != "pkgs") self.overlays;
+	overlayPkgs =
+	  lib.genAttrs
+	    (attrNames overlays)
+	    (name: (overlays."${name}" pkgs pkgs)."${name}");
+      in
+      overlayPkgs;
+
 
     nixosConfigurations = {
-      nixos = inputs.unstable.lib.nixosSystem {
+      nixos = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/x542ur/configuration.nix
+
+	  # Home Manager
 	  home-manager.nixosModules.home-manager
 	  {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 	    home-manager.users.me = import ./home/home.nix;
 	  }
-	  {nixpkgs.overlays = [self.overlays.neovim];}
+
+	  # Overlays
+	  {
+            nixpkgs.overlays =
+              let
+                overlays = map
+                  (name: import (./overlays + "/${name}"))
+                  (attrNames (readDir ./overlays));
+              in
+              overlays;
+	  }
 	  unstable.nixosModules.notDetected
         ];
       };
