dbb153dd70784cea2dbc51d75144c1fce7c19e81
ishan <ishan365.ia@gmail.com>
2020-10-29 08:45:53 +0530

    fix the strings assigned.

--- user/programs/mpv/default.nix
+++ user/programs/mpv/default.nix
@@ -2,9 +2,9 @@
   programs.mpv = {
     enable = true;
     config = {
-      hwdec = auto;
-      vo = gpu;
-      gpu-context = wayland;
+      hwdec = "auto";
+      vo = "gpu";
+      gpu-context = "wayland";
     };
   };
 }
