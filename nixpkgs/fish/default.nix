b2dd86266011bf2dd9036dbdad780e2c51fa401c
ishan <ishan365.ia@gmail.com>
2020-11-19 15:25:44 +0530

    add quit on cd for nnn in fish

--- nixpkgs/fish/default.nix
+++ nixpkgs/fish/default.nix
@@ -6,5 +6,32 @@
     promptInit = ''
       ${pkgs.starship}/bin/starship init fish | source
     '';
+    interactiveShellInit = ''
+
+      #------------ cd on quit for nnn -------------+
+
+      function n --wraps nnn --description 'support nnn quit and change directory'
+          # Block nesting of nnn in subshells
+          if test -n "$NNNLVL"
+              if [ (expr $NNNLVL + 0) -ge 1 ]
+                  echo "nnn is already running"
+                  return
+              end
+          end
+
+          if test -n "$XDG_CONFIG_HOME"
+              set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+          else
+              set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+          end
+
+          nnn $argv
+
+          if test -e $NNN_TMPFILE
+              source $NNN_TMPFILE
+              rm $NNN_TMPFILE
+          end
+      end
+          '';
   };
 }
