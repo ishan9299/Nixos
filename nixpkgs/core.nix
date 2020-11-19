7a057dca4639ab4bf31e8a2b22cf5c7a8e10adde
ishan <ishan365.ia@gmail.com>
2020-11-19 10:42:04 +0530

    added nnn plugin env variable

--- nixpkgs/core.nix
+++ nixpkgs/core.nix
@@ -23,6 +23,7 @@
            export NNN_TRASH=1
            export NNN_FIFO=/tmp/nnn.fifo
            export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)"
+           export NNN_PLUG="p:preview-tui"
          '';
 
     systemPackages = with pkgs; [
