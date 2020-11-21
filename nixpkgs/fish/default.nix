5510242058842a6200bf57014c6724a085f7acf4
ishan <ishan365.ia@gmail.com>
2020-11-21 13:44:05 +0530

    Cleanup the fish config file

--- nixpkgs/fish/default.nix
+++ nixpkgs/fish/default.nix
@@ -1,4 +1,6 @@
-{ lib, pkgs, ... }: {
+{ lib, pkgs, ... }:
+let inherit (builtins) readFile;
+in {
   users.defaultUserShell = pkgs.fish;
 
   programs.fish = {
@@ -9,29 +11,7 @@
     interactiveShellInit = ''
 
       #------------ cd on quit for nnn -------------+
-
-      function n --wraps nnn --description 'support nnn quit and change directory'
-          # Block nesting of nnn in subshells
-          if test -n "$NNNLVL"
-              if [ (expr $NNNLVL + 0) -ge 1 ]
-                  echo "nnn is already running"
-                  return
-              end
-          end
-
-          if test -n "$XDG_CONFIG_HOME"
-              set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
-          else
-              set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
-          end
-
-          nnn $argv
-
-          if test -e $NNN_TMPFILE
-              source $NNN_TMPFILE
-              rm $NNN_TMPFILE
-          end
-      end
-          '';
+      ${readFile ./functions/n.fish}
+    '';
   };
 }
