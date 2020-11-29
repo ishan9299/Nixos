61cd76f97de09b26d9bb1b4e352bf9a71f33fa50
ishan <ishan365.ia@gmail.com>
2020-11-29 14:22:49 +0530

    turn of syntax highlighting for bat it is painful to deal with

--- user/programs/bat/default.nix
+++ user/programs/bat/default.nix
@@ -1,6 +1,9 @@
 {
   programs.bat = {
     enable = true;
-    config = { theme = "Solarized (light)"; };
+    config = {
+      theme = "Solarized (light)";
+      color = "never";
+    };
   };
 }
