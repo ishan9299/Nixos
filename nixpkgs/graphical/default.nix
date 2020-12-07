b811ef97c5f77c14bad6252bdb50ffff607c84be
ishan <ishan365.ia@gmail.com>
2020-12-07 19:29:26 +0530

    sway: update config

--- nixpkgs/graphical/default.nix
+++ nixpkgs/graphical/default.nix
@@ -22,7 +22,7 @@
     qemuGuest.enable = true;
     tlp.enable = true;
     gnome3.gnome-remote-desktop.enable = false;
-   };
+  };
 
   # Also add sway
   programs.sway = {
@@ -34,9 +34,18 @@
       waybar # status bar
       mako # notification daemon
       kanshi # autorandr
-      autotiling
+      wl-clipboard # clipboard
+      autotiling # dynamic tiling in sway
+      brightnessctl # brightness control
+      nwg-launchers # launcher
+      polkit_gnome
     ];
   };
+
+  programs.waybar.enable = true;
+
+  # Not strictly required but pipewire will use rtkit if it is present
+  security.rtkit.enable = true;
 
   environment = {
     etc = {
@@ -45,16 +54,23 @@
       "xdg/waybar/config".source = ./waybar/config;
       "xdg/waybar/style.css".source = ./waybar/style.css;
     };
-  };
 
-
-
-  # Not strictly required but pipewire will use rtkit if it is present
-  security.rtkit.enable = true;
-
-  environment = {
     # Extra gui packages
     systemPackages = with pkgs; [
+
+      (pkgs.writeTextFile {
+        name = "startsway";
+        destination = "/bin/startsway";
+        executable = true;
+        text = ''
+          #! ${pkgs.bash}/bin/bash
+
+          # first import environment variables from the login manager
+            systemctl --user import-environment
+          # then start the service
+            exec systemctl --user start sway.service
+        '';
+      })
 
       # Browser
       google-chrome
@@ -96,6 +112,58 @@
     ];
   };
 
+  # needed for redshift
+  location.provider = "geoclue2";
+
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor session";
+    documentation = [ "man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.services.sway = {
+    description = "Sway - Wayland window manager";
+    documentation = [ "man:sway(5)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+    # We explicitly unset PATH here, as we want it to be set by
+    # systemctl --user import-environment in startsway
+    environment.PATH = lib.mkForce null;
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+      '';
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  services.redshift = {
+    enable = true;
+    # Redshift with wayland support isn't present in nixos-19.09 atm. You have to cherry-pick the commit from https://github.com/NixOS/nixpkgs/pull/68285 to do that.
+    package = pkgs.redshift-wlr;
+  };
+
+  systemd.user.services.kanshi = {
+    description = "Kanshi output autoconfig ";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      # kanshi doesn't have an option to specifiy config file yet, so it looks
+      # at .config/kanshi/config
+      ExecStart = ''
+        ${pkgs.kanshi}/bin/kanshi
+      '';
+      RestartSec = 5;
+      Restart = "always";
+    };
+  };
+
   hardware = {
     enableAllFirmware = true;
     pulseaudio.enable = true;
@@ -116,9 +184,13 @@
     source-sans-pro
     source-serif-pro
     (nerdfonts.override {
-      fonts = [ "CascadiaCode" "FiraCode" "Overpass" "Iosevka" "JetBrainsMono" ];
+      fonts =
+        [ "CascadiaCode" "FiraCode" "Overpass" "Iosevka" "JetBrainsMono" ];
     })
   ];
+
+  # Qt Stuff
+  programs.qt5ct.enable = true;
 
   # Flatpak
   services.flatpak.enable = true;
