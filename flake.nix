521bc0c00f6614f7b1d4afed83a6f320c3aedbe6
ishan <ishan365.ia@gmail.com>
2020-12-08 16:56:16 +0530

    nixos: update flake.nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       flake = false;
     };
     nixpkgs-wayland = {
-      url = "github:ishan9299/nixpkgs-wayland";
+      url = "github:colemickens/nixpkgs-wayland";
       inputs.nixpkgs.follows = "unstable";
     };
   };
