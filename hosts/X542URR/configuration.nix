9186b7dcffe901b5fc938ba71d6b9ea868143c69
ishan <ishan365.ia@gmail.com>
2020-12-25 07:06:15 +0530

    hosts: X542URR: move to system.stateVersion to config attr

--- hosts/X542URR/configuration.nix
+++ hosts/X542URR/configuration.nix
@@ -72,13 +72,13 @@ in {
       imports =
         (map (name: ../../user + "/${name}")) (attrNames (readDir ../../user));
     };
-  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "20.09"; # Did you read the comment?
+  };
 }
