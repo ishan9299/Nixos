0edd7330f9355cb9290f55c22973da54199f60d7
ishan <ishan365.ia@gmail.com>
2020-12-25 15:12:01 +0530

    profiles: develop: fish: add nnn settings

--- profiles/develop/fish/default.nix
+++ profiles/develop/fish/default.nix
@@ -8,10 +8,18 @@ in {
     promptInit = ''
       ${pkgs.starship}/bin/starship init fish | source
     '';
+
     interactiveShellInit = ''
 
       #------------ Disable the greeting -----------+
       set fish_greeting
+
+      #---- NNN settings
+      export NNN_OPTS="eRHdF"
+      export NNN_TRASH=1
+      export NNN_FIFO=/tmp/nnn.fifo
+      export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)"
+      export NNN_PLUG="p:preview-tui"
 
       #------------ cd on quit for nnn -------------+
       ${readFile ./functions/n.fish}
