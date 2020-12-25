9582a99b7d928fa61fb9aef9c3fa07e9ddd32311
ishan <ishan365.ia@gmail.com>
2020-12-25 15:27:20 +0530

    overlays: nnn: update

--- overlays/nnn.nix
+++ overlays/nnn.nix
@@ -1,11 +1,5 @@
 final: prev: {
   nnn = prev.nnn.overrideAttrs (oldAttrs: {
-    src = prev.fetchFromGitHub {
-      owner = "jarun";
-      repo = "nnn";
-      rev = "3.5";
-      sha256 = "1fa7cmwrzn6kx87kms8i98p9azdlwyh2gnif29l340syl9hkr5qy";
-    };
     makeFlags = oldAttrs.makeFlags ++ [ "O_NERD=1" ];
   });
 }
