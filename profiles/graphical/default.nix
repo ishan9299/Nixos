b1922bde7c136b2e1d41721d1345da8e58c1c878
ishan <ishan365.ia@gmail.com>
2020-12-26 12:51:39 +0530

    profiles: graphical: disable nvidia optimus

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -1,31 +1,34 @@
 { config, pkgs, lib, ... }:
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec -a "$0" "$@"
-  '';
-in {
+# let
+#   nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+#     export __NV_PRIME_RENDER_OFFLOAD=1
+#     export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+#     export __GLX_VENDOR_LIBRARY_NAME=nvidia
+#     export __VK_LAYER_NV_optimus=NVIDIA_only
+#     exec -a "$0" "$@"
+#   '';
+# in 
+{
   imports = [ ./gnome ./sway ];
-  services.xserver.displayManager.gdm.nvidiaWayland = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+#  services.xserver.displayManager.gdm.nvidiaWayland = true;
+#  services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware = {
-    nvidia = {
-      modesetting.enable = true;
-      prime = {
-        offload.enable = true;
-        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-        intelBusId = "PCI:0:2:0";
-        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
-  };
+#  hardware = {
+#    nvidia = {
+#      modesetting.enable = true;
+#      prime = {
+#        offload.enable = true;
+#        # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+#        intelBusId = "PCI:0:2:0";
+#        # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+#        nvidiaBusId = "PCI:1:0:0";
+#      };
+#    };
+#  };
 
-  environment.systemPackages = with pkgs; [ nvidia-offload ];
+#  environment.systemPackages = with pkgs; [ nvidia-offload ];
+
+  hardware.nvidiaOptimus.disable = true;
 
   fonts.fonts = with pkgs; [
     corefonts
