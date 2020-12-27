1de79512f07857002de60bf29dd0ee4898c0480a
ishan <ishan365.ia@gmail.com>
2020-12-27 07:50:45 +0530

    user: waybar: update the configuration

--- user/waybar/default.nix
+++ user/waybar/default.nix
@@ -13,21 +13,75 @@
       ];
 
       modules-center = [
+        "clock"
       ];
 
       modules-right = [
+        "pulseaudio"
+        "network"
+        "battery"
       ];
 
       modules = {
-        "sway/mode" = { tooltip = false; };
+        "sway/mode" = {
+          "format" = "<span style=\"italic\">{}</span>";
+        };
         "sway/workspaces" = {
+          "disable-scroll" = true;
+          "all-outputs" = true;
           "persistent_workspaces" = {
             "1" = [];
             "2" = [];
             "3" = [];
             "4" = [];
+            "5" = [];
           };
         };
+        "clock" = {
+          "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          "format-alt" = "{:%Y-%m-%d}";
+        };
+
+        "pulseaudio" = {
+          "format" = "{volume}% {icon} {format_source}";
+          "format-bluetooth" = "{volume}% {icon} {format_source}";
+          "format-bluetooth-muted" = " {icon} {format_source}";
+          "format-muted" = " {format_source}";
+          "format-source" = "{volume}% ";
+          "format-source-muted" = "";
+          "format-icons" = {
+              "headphone" = "";
+              "hands-free" = "";
+              "headset" = "";
+              "phone" = "";
+              "portable" = "";
+              "car" = "";
+              "default" = ["" "" ""];
+          };
+          "on-click" = "pavucontrol";
+        };
+
+        "network" = {
+          "format-wifi" = "{essid} ({signalStrength}%) ";
+          "format-ethernet" = "{ifname} = {ipaddr}/{cidr} ";
+          "format-linked" = "{ifname} (No IP) ";
+          "format-disconnected" = "Disconnected ⚠";
+          "format-alt" = "{ifname} = {ipaddr}/{cidr}";
+        };
+
+        "battery" = {
+          "states" = {
+              "good" = 95;
+              "warning" = 30;
+              "critical" = 15;
+          };
+          "format" = "{capacity}% {icon}";
+          "format-charging" = "{capacity}% ";
+          "format-plugged" = "{capacity}% ";
+          "format-alt" = "{time} {icon}";
+          "format-icons" = ["" "" "" "" ""];
+        };
+
       };
     }];
   };
