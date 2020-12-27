b0d121c6a6aff4419323a2cc19da0c98af0a0f33
ishan <ishan365.ia@gmail.com>
2020-12-27 12:55:45 +0530

    user: waybar: update the config

--- user/waybar/default.nix
+++ user/waybar/default.nix
@@ -7,56 +7,75 @@
       position = "top";
       height = 30;
 
-      modules-left = [
-        "sway/mode"
-        "sway/workspaces"
-      ];
+      modules-left = [ "sway/mode" "sway/workspaces" ];
 
-      modules-center = [
-        "clock"
-      ];
+      modules-center = [ "clock" ];
 
-      modules-right = [
-        "pulseaudio"
-        "network"
-        "battery"
-      ];
+      modules-right = [ "pulseaudio" "network" "battery" ];
 
       modules = {
-        "sway/mode" = {
-          "format" = "<span style=\"italic\">{}</span>";
-        };
+        "sway/mode" = { "format" = ''<span style="italic">{}</span>''; };
         "sway/workspaces" = {
           "disable-scroll" = true;
           "all-outputs" = true;
           "persistent_workspaces" = {
-            "1" = [];
-            "2" = [];
-            "3" = [];
-            "4" = [];
-            "5" = [];
+            "1" = [ ];
+            "2" = [ ];
+            "3" = [ ];
+            "4" = [ ];
+            "5" = [ ];
           };
+          "format-icons" = {
+            "1" = "𑁧";
+            "2" = "𑁨";
+            "3" = "𑁩";
+            "4" = "𑁪";
+            "5" = "𑁫";
+            "focused" = "";
+            "urgent" = "";
+            "default" = "";
+          };
+          "icon-size" = 15;
         };
         "clock" = {
-          "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          "tooltip-format" = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
           "format-alt" = "{:%Y-%m-%d}";
         };
 
         "pulseaudio" = {
-          "format" = "{volume}% {icon} {format_source}";
+          "scroll-step" = 1; # %; can be a float
+          "format" = "{icon}";
           "format-bluetooth" = "{volume}% {icon} {format_source}";
           "format-bluetooth-muted" = " {icon} {format_source}";
           "format-muted" = " {format_source}";
           "format-source" = "{volume}% ";
           "format-source-muted" = "";
           "format-icons" = {
-              "headphone" = "";
-              "hands-free" = "";
-              "headset" = "";
-              "phone" = "";
-              "portable" = "";
-              "car" = "";
-              "default" = ["" "" ""];
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = [
+              ""
+              "   -"
+              "   --"
+              "   ---"
+              "   ----"
+              "   -----"
+              "   ------"
+              "   -------"
+              "   --------"
+              "   ---------"
+              "   ----------"
+              "   -----------"
+              "   ------------"
+              "   -------------"
+              "   --------------"
+            ];
           };
           "on-click" = "pavucontrol";
         };
@@ -71,15 +90,15 @@
 
         "battery" = {
           "states" = {
-              "good" = 95;
-              "warning" = 30;
-              "critical" = 15;
+            "good" = 95;
+            "warning" = 30;
+            "critical" = 15;
           };
           "format" = "{capacity}% {icon}";
           "format-charging" = "{capacity}% ";
           "format-plugged" = "{capacity}% ";
           "format-alt" = "{time} {icon}";
-          "format-icons" = ["" "" "" "" ""];
+          "format-icons" = [ "" "" "" "" "" ];
         };
 
       };
