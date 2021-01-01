3860c6a63113b07d1f549202c764fcf58e096346
ishan <ishan365.ia@gmail.com>
2021-01-01 17:20:00 +0530

    direnv: some settings for direnv

--- user/direnv/default.nix
+++ user/direnv/default.nix
@@ -1,15 +1,14 @@
 {
   programs.direnv = {
     enable = true;
-    enableFishIntegration = true;
-    enableBashIntegration = true;
-    enableNixDirenvIntegration = true;
-    stdlib = ''
-      use_flake() {
-      watch_file flake.nix
-      watch_file flake.lock
-      eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-      }
-    '';
+    # programs.direnv.enableFishIntegration = true;
+    # enableNixDirenvIntegration = true;
+    # stdlib = "
+    # use_flake() {
+    #   watch_file flake.nix
+    #   watch_file flake.lock
+    #   eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+    # }
+    ";
   };
 }
