7184d512a434e97967ea59af00503a95ba40e4a6
ishan <ishan365.ia@gmail.com>
2021-01-01 21:36:32 +0530

    direnv: works with unstable nix

--- user/direnv/default.nix
+++ user/direnv/default.nix
@@ -1,14 +1,14 @@
 {
   programs.direnv = {
     enable = true;
-    # programs.direnv.enableFishIntegration = true;
-    # enableNixDirenvIntegration = true;
-    # stdlib = "
-    # use_flake() {
-    #   watch_file flake.nix
-    #   watch_file flake.lock
-    #   eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-    # }
+    enableFishIntegration = true;
+    enableNixDirenvIntegration = true;
+    stdlib = "
+    use_flake() {
+      watch_file flake.nix
+      watch_file flake.lock
+      eval \"$(nix print-dev-env --profile \"$(direnv_layout_dir)/flake-profile\")\"
+    }
     ";
   };
 }
