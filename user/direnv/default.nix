6bb75baa0abf5dda3c145f656d340e3f8c7cbd75
ishan <ishan365.ia@gmail.com>
2021-01-01 06:24:43 +0530

    direnv: add support for use_nix and use_flake

--- user/direnv/default.nix
+++ user/direnv/default.nix
@@ -3,5 +3,13 @@
     enable = true;
     enableFishIntegration = true;
     enableBashIntegration = true;
+    enableNixDirenvIntegration = true;
+    stdlib = ''
+      use_flake() {
+      watch_file flake.nix
+      watch_file flake.lock
+      eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
+      }
+    '';
   };
 }
