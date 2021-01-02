e2ee6e67873fec9fe31b14d320fa1664a1795c06
ishan <ishan365.ia@gmail.com>
2021-01-02 17:05:00 +0530

    graphical: sway: changes

--- profiles/graphical/sway/default.nix
+++ profiles/graphical/sway/default.nix
@@ -3,6 +3,15 @@
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      # needs qt5.qtwayland in systemPackages
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      # Fix for some Java AWT applications (e.g. Android Studio),
+      # use this if they aren't displayed properly:
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
     extraPackages = with pkgs; [
       swaylock # lockscreen
       swayidle
@@ -21,6 +30,14 @@
     ];
   };
 
+  services.xserver = {
+    enable = true;
+    libinput.enable = true;
+    displayManager.sddm = {
+      enable = true;
+    };
+  };
+
   environment = {
     etc = {
       # Put config files in /etc. Note that you also can put these in ~/.config, but then you can't manage them with NixOS anymore!
@@ -29,25 +46,6 @@
       "xdg/waybar/config".source = ./dotfiles/waybar/config;
       "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
     };
-
-    # Extra gui packages
-    systemPackages = with pkgs; [
-
-      (pkgs.writeTextFile {
-        name = "startsway";
-        destination = "/bin/startsway";
-        executable = true;
-        text = ''
-          #! ${pkgs.bash}/bin/bash
-
-          # first import environment variables from the login manager
-            systemctl --user import-environment
-            systemctl --user start sway-session.target
-          # then start the service
-            exec systemctl --user start sway.service
-        '';
-      })
-    ];
   };
 
   # needed for redshift
@@ -61,37 +59,9 @@
     after = [ "graphical-session-pre.target" ];
   };
 
-  systemd.user.services.sway = {
-    description = "Sway - Wayland window manager";
-    documentation = [ "man:sway(5)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-    # We explicitly unset PATH here, as we want it to be set by
-    # systemctl --user import-environment in startsway
-    # environment.PATH = lib.mkForce null;
-    environment.PATH = lib.mkForce null;
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-      '';
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
-
   services.redshift = {
     enable = true;
     package = pkgs.redshift-wlr;
-  };
-
-  systemd.user.services.waybar = {
-    description = "Waybar as systemd service";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    script = "${pkgs.waybar}/bin/waybar";
   };
 
   systemd.user.services.kanshi = {
@@ -99,8 +69,6 @@
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
     serviceConfig = {
-      # kanshi doesn't have an option to specifiy config file yet, so it looks
-      # at .config/kanshi/config
       ExecStart = ''
         ${pkgs.kanshi}/bin/kanshi
       '';
