dffeaf1538949d6083e086ababe39eb0353e60e2
ishan <ishan365.ia@gmail.com>
2021-01-03 06:04:05 +0530

    direnv: change " " to '' ''

--- user/direnv/default.nix
+++ user/direnv/default.nix
@@ -4,12 +4,12 @@
     enableFishIntegration = true;
     enableBashIntegration = true;
     enableNixDirenvIntegration = true;
-    stdlib = "
+    stdlib = ''
     use_flake() {
       watch_file flake.nix
       watch_file flake.lock
-      eval \"$(nix print-dev-env --profile \"$(direnv_layout_dir)/flake-profile\")\"
+      eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
     }
-    ";
+    '';
   };
 }
