754e1ca53322ed23bba422eebd2f03de220d287e
ishan <ishan365.ia@gmail.com>
2021-01-25 08:43:50 +0530

    change dotfiles to configs

--- profiles/graphical/sway/default.nix
+++ profiles/graphical/sway/default.nix
@@ -5,11 +5,8 @@
     wrapperFeatures = { gtk = true; };
     extraSessionCommands = ''
       export SDL_VIDEODRIVER=wayland
-      # needs qt5.qtwayland in systemPackages
       export QT_QPA_PLATFORM=wayland
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      # Fix for some Java AWT applications (e.g. Android Studio),
-      # use this if they aren't displayed properly:
       export _JAVA_AWT_WM_NONREPARENTING=1
     '';
     extraPackages = with pkgs; [
@@ -24,22 +21,21 @@
       font-awesome # waybar icons
       nwg-launchers # launcher
       wofi # launcher
-      grim
-      slurp
+      grim # screenshot
+      slurp # screenshot
       polkit_gnome
       waybar # bar
       wlogout
-      pavucontrol
     ];
   };
 
   environment = {
     etc = {
       # Put config files in /etc. Note that you also can put these in ~/.config, but then you can't manage them with NixOS anymore!
-      "sway/config".source = ./dotfiles/sway/config;
-      "sway/scripts/swayworkspace".source = ./dotfiles/sway/scripts/swayworkspace;
-      "xdg/waybar/config".source = ./dotfiles/waybar/config;
-      "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
+      "sway/config".source = ./configs/sway/config;
+      "sway/scripts/swayworkspace".source = ./configs/sway/scripts/swayworkspace;
+      "xdg/waybar/config".source = ./configs/waybar/config;
+      "xdg/waybar/style.css".source = ./configs/waybar/style.css;
       };
 
     systemPackages = with pkgs; [
@@ -49,39 +45,10 @@
       gtk-engine-murrine
       gtk_engines
       gsettings-desktop-schemas
-      lxappearance
+      pavucontrol
     ];
   };
 
   # needed for redshift
   location.provider = "geoclue2";
-
-  systemd.user.targets.sway-session = {
-    description = "sway compositor session";
-    documentation = [ "man:systemd.special(7)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-    requiredBy = [ "graphical-session.target" "graphical-session-pre.target" ];
-  };
-
-  services.redshift = {
-    enable = true;
-    package = pkgs.redshift-wlr;
-  };
-
-  systemd.user.services.kanshi = {
-    enable = true;
-    description = "Kanshi output autoconfig ";
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = ''
-        ${pkgs.kanshi}/bin/kanshi
-      '';
-      RestartSec = 5;
-      Restart = "always";
-    };
-  };
-
 }
