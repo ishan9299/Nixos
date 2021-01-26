d3d62b7f6e488b534ff7b0473755da4cfd9b33b1
ishan <ishan365.ia@gmail.com>
2021-01-26 22:33:20 +0530

    set the xdg_current_desktop variable

--- profiles/graphical/sway/default.nix
+++ profiles/graphical/sway/default.nix
@@ -8,6 +8,7 @@
       export QT_QPA_PLATFORM=wayland
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
       export _JAVA_AWT_WM_NONREPARENTING=1
+      export XDG_CURRENT_DESKTOP=sway
     '';
     extraPackages = with pkgs; [
       swaylock # lockscreen
