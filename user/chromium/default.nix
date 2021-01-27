f269496f9cab28b7837cf4df2c0c550f113dc706
ishan <ishan365.ia@gmail.com>
2021-01-27 14:53:32 +0530

    chromium

--- user/chromium/default.nix
+++ user/chromium/default.nix
@@ -2,7 +2,7 @@
 {
   programs.chromium = {
     enable = true;
-    package = pkgs.chromiumDev;
+    package = pkgs.ungoogled-chromium;
     extensions = [
       { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # vimium
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock-origin
