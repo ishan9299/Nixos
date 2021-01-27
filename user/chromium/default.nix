30d9577c358c49dfe0e3dc7d5dc4a04b8f5bd25c
ishan <ishan365.ia@gmail.com>
2021-01-27 14:34:03 +0530

    add chromium package

---
+++ user/chromium/default.nix
@@ -0,0 +1,12 @@
+{
+  programs.chromium = {
+    enable = true;
+    package = pkgs.chromiumDev;
+    extensions = [
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # vimium
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock-origin
+      { id = "gcbommkclmclpchllfjekcdonpmejbdp"; } # https-everywhere
+      { id = "ebamaffgiechnomghfojkmlkaipoadni"; } # hide-likes
+    ];
+  }
+}
