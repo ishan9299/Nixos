20a13ccb5c69de6b646b4745b8cccf64bf73acef
ishan <ishan365.ia@gmail.com>
2021-01-29 08:13:15 +0530

    firefox: disable

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -1,7 +1,7 @@
-{config, pkgs, lib, ...}:
-{
+{ config, pkgs, lib, ... }: {
   programs.firefox = {
-    enable = true;
+    enable = false;
+    package = pkgs.firefox-bin;
     extensions = lib.mkIf config.programs.firefox.enable
       (with pkgs.nur.repos.rycee.firefox-addons; [
         https-everywhere
