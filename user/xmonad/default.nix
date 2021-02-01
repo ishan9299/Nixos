23c76151fd5969953ca386064426c619f09dc015
ishan <ishan365.ia@gmail.com>
2021-02-01 21:34:57 +0530

    add a sample xmonad config

--- user/xmonad/default.nix
+++ user/xmonad/default.nix
@@ -1,14 +1,10 @@
-{
-  xsession.windowManager.xmonad = {
+{ pkgs, ... }: {
+  xsession = {
     enable = true;
-    enableContribAndExtras = true;
-    config = pkgs.writeText "xmonad.hs" ''
-      import XMonad
-      main = xmonad defaultConfig
-      { terminal = "urxvt"
-      , modMask = mod4Mask
-      , borderWidth = 3
-      }
-      ''
-  }
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      config = ./xmonad.hs;
+    };
+  };
 }
