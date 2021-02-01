5ed808c63ae57f2e02099dc3b618ed8b14538528
ishan <ishan365.ia@gmail.com>
2021-02-01 21:02:36 +0530

    remove dwm

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./gnome
     #  ./sway
-    ./dwm
     # ./plasma
   ];
 
--- profiles/graphical/dwm/default.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-  services.xserver.windowManager.dwm.enable = true;
-  services.dwm-status.enable = true;
-}
