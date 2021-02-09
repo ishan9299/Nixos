9947afb4b10744a2d28942c9d64916ca17837b0a
ishan <ishan365.ia@gmail.com>
2021-02-09 12:04:15 +0530

    add some stuff for switcherooctl

--- profiles/graphical/gnome/default.nix
+++ profiles/graphical/gnome/default.nix
@@ -51,4 +51,10 @@
   };
 
   security.hideProcessInformation = false;
+
+  # switcheroo-control stuff
+  services.dbus.packages = [ pkgs.switcheroo-control ];
+  environment.systemPackages = [ pkgs.switcheroo-control ];
+  systemd.packages = [ pkgs.switcheroo-control ];
+  systemd.targets.multi-user.wants = [ "switcheroo-control.service" ];
 }
