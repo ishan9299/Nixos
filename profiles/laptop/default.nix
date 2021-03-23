ba5acaee8dd0f3ff17515f60fd8e6e59d5ef2ac1
ishan <ishan365.ia@gmail.com>
2021-03-23 08:40:36 +0530

    disable optimus for gvt-g

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -18,18 +18,18 @@ in
     usbutils
   ];
 
-  #  hardware.nvidiaOptimus.disable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidiaOptimus.disable = true;
   hardware.nvidia = {
-    nvidiaPersistenced = true;
-    prime = {
-      offload.enable = true;
-      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-      intelBusId = "PCI:0:2:0";
-      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-      nvidiaBusId = "PCI:1:0:0";
-    };
-    powerManagement.finegrained = true;
+  #   nvidiaPersistenced = true;
+  #   prime = {
+  #     offload.enable = true;
+  #     # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+  #     intelBusId = "PCI:0:2:0";
+  #     # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+  #     nvidiaBusId = "PCI:1:0:0";
+  #   };
+  #   powerManagement.finegrained = true;
     modesetting.enable = true;
   };
 
@@ -44,12 +44,12 @@ in
   security.rtkit.enable = true;
   # To use this disable pulseaudio and make sure
   # the user is in the audio group.
-  # services.pipewire = {
-  #   enable = true;
-  #   socketActivation = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   jack.enable = true;
-  #   pulse.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    socketActivation = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+    pulse.enable = true;
+  };
 }
