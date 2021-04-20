b116f851daadbf5adaaa81a92e4f798abea6fce6
ishan <ishan365.ia@gmail.com>
2021-04-20 05:38:08 +0530

    changes to the fish prompt

--- profiles/develop/fish/functions/fish_prompt.fish
+++ profiles/develop/fish/functions/fish_prompt.fish
@@ -8,19 +8,19 @@ function fish_prompt
 	set -g __fish_git_prompt_show_informative_status 1
 	set -g __fish_git_prompt_hide_untrackedfiles 1
 
-	set -g __fish_git_prompt_color_branch magenta --bold
+	set -g __fish_git_prompt_color_branch red --bold
 	set -g __fish_git_prompt_showupstream "informative"
 	set -g __fish_git_prompt_char_upstream_ahead "↑"
 	set -g __fish_git_prompt_char_upstream_behind "↓"
 	set -g __fish_git_prompt_char_upstream_prefix ""
 
-	set -g __fish_git_prompt_char_stagedstate "●"
-	set -g __fish_git_prompt_char_dirtystate "✚"
-	set -g __fish_git_prompt_char_untrackedfiles "…"
-	set -g __fish_git_prompt_char_conflictedstate "✖"
-	set -g __fish_git_prompt_char_cleanstate "✔"
+	set -g __fish_git_prompt_char_stagedstate "+"
+	set -g __fish_git_prompt_char_dirtystate "!"
+	set -g __fish_git_prompt_char_untrackedfiles "?"
+	set -g __fish_git_prompt_char_conflictedstate "="
+	set -g __fish_git_prompt_char_cleanstate ""
 
-	set -g __fish_git_prompt_color_dirtystate blue
+	set -g __fish_git_prompt_color_dirtystate red
 	set -g __fish_git_prompt_color_stagedstate yellow
 	set -g __fish_git_prompt_color_invalidstate red
 	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
@@ -31,8 +31,7 @@ function fish_prompt
 		 (tput bold)$USER@(hostname) \
 		 (set_color normal --bold)on \
 		 (set_color yellow --bold)\[(date '+%Y-%m-%d %H:%M:%S')\] \
-		 (set_color cyan --bold)(prompt_pwd) \
-		 (set_color purple --bold)(fish_git_prompt) \
+		 (set_color cyan --bold)(prompt_pwd)(fish_git_prompt) \
 		 "$nix_shell_info"
 	echo -n (set_color blue --bold)'#:'(set_color normal)
 end
