2f0784ef8fc4f72508036053f4a2e1883eff4ab2
ishan <ishan365.ia@gmail.com>
2021-05-15 13:59:18 +0530

    fix the color scheme for fish

--- profiles/develop/fish/functions/__colors.fish
+++ profiles/develop/fish/functions/__colors.fish
@@ -1,31 +1,30 @@
 # This file contains fish universal variable definitions.
-# VERSION: 3.0
-SETUVAR __fish_initialized:3100
-SETUVAR fish_color_autosuggestion:7596E4
-SETUVAR fish_color_cancel:\x2dr
-SETUVAR fish_color_command:164CC9
-SETUVAR fish_color_comment:007B7B
-SETUVAR fish_color_cwd:green
-SETUVAR fish_color_cwd_root:red
-SETUVAR fish_color_end:02BDBD
-SETUVAR fish_color_error:9177E5
-SETUVAR fish_color_escape:00a6b2
-SETUVAR fish_color_history_current:\x2d\x2dbold
-SETUVAR fish_color_host:normal
-SETUVAR fish_color_host_remote:A37ACC
-SETUVAR fish_color_match:\x2d\x2dbackground\x3dbrblue
-SETUVAR fish_color_normal:normal
-SETUVAR fish_color_operator:00a6b2
-SETUVAR fish_color_param:4319CC
-SETUVAR fish_color_quote:4C3499
-SETUVAR fish_color_redirection:248E8E
-SETUVAR fish_color_search_match:bryellow\x1e\x2d\x2dbackground\x3dbrblack
-SETUVAR fish_color_selection:white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
-SETUVAR fish_color_status:red
-SETUVAR fish_color_user:brgreen
-SETUVAR fish_color_valid_path:\x2d\x2dunderline
-SETUVAR fish_key_bindings:fish_default_key_bindings
-SETUVAR fish_pager_color_completion:normal
-SETUVAR fish_pager_color_description:B3A06D\x1eyellow
-SETUVAR fish_pager_color_prefix:white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
-SETUVAR fish_pager_color_progress:brwhite\x1e\x2d\x2dbackground\x3dcyan
+# VERSION  3.0
+set --universal fish_color_autosuggestion 7596E4
+set --universal fish_color_cancel \x2dr
+set --universal fish_color_command 164CC9
+set --universal fish_color_comment 007B7B
+set --universal fish_color_cwd green
+set --universal fish_color_cwd_root red
+set --universal fish_color_end 02BDBD
+set --universal fish_color_error 9177E5
+set --universal fish_color_escape 00a6b2
+set --universal fish_color_history_current \x2d\x2dbold
+set --universal fish_color_host normal
+set --universal fish_color_host_remote A37ACC
+set --universal fish_color_match \x2d\x2dbackground\x3dbrblue
+set --universal fish_color_normal normal
+set --universal fish_color_operator 00a6b2
+set --universal fish_color_param 4319CC
+set --universal fish_color_quote 4C3499
+set --universal fish_color_redirection 248E8E
+set --universal fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3dbrblack
+set --universal fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+set --universal fish_color_status red
+set --universal fish_color_user brgreen
+set --universal fish_color_valid_path \x2d\x2dunderline
+set --universal fish_key_bindings fish_default_key_bindings
+set --universal fish_pager_color_completion normal
+set --universal fish_pager_color_description B3A06D\x1eyellow
+set --universal fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
+set --universal fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
