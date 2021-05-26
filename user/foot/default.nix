38f48629bc71b33168940c870e17cb5c32821eb1
ishan <ishan365.ia@gmail.com>
2021-05-26 12:53:41 +0530

    change the font

--- user/foot/default.nix
+++ user/foot/default.nix
@@ -4,10 +4,10 @@
     server.enable = false;
     settings = {
       main = {
-        font="Iosevka Nerd Font:size=11";
-        font-bold="Iosevka Nerd Font:size=11";
-        font-italic="Iosevka Nerd Font:size=11:slant=italic";
-        font-bold-italic="Iosevka Nerd Font:size=11:slant=italic";
+        font="Input Mono:size=11";
+        font-bold="Input Mono:size=11";
+        font-italic="Input Mono:size=11:slant=italic";
+        font-bold-italic="Input Mono:size=11:slant=italic";
         box-drawings-uses-font-glyphs="yes";
         pad="0x0 center";
         term="xterm-256color";
