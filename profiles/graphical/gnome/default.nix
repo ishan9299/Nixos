aa3f4589f76c68decc25e2169014d4bb152002ea
ishan <ishan365.ia@gmail.com>
2021-06-01 18:02:35 +0530

    fix the gnome config

--- profiles/graphical/gnome/default.nix
+++ profiles/graphical/gnome/default.nix
@@ -1,10 +1,56 @@
+{ pkgs, config, ... }:
 {
   services = {
-    xserver.displayManager.gdm.enable = true;
-    xserver.displayManager.defaultSession = "gnome";
-    xserver.desktopManager = {
-      gnome.enable = true;
+    xserver = {
+      enable = true;
+      layout = "us"; # keyboard layout
+      # Enable GDM
+      desktopManager = {
+        gnome.enable = true;
+        xterm.enable = true;
+      };
+      displayManager = {
+        gdm = {
+          enable = true;
+          wayland = true;
+          nvidiaWayland = true;
+        };
+      };
+      # Enable TouchInputs
+      libinput.enable = true;
     };
-    gnome.gnome-keyring.enable = true;
+    gnome = {
+      gnome-keyring.enable = true;
+      sushi.enable = true;
+      gnome-user-share.enable = true;
+      core-shell.enable = true;
+      core-os-services.enable = true;
+    };
+    switcherooControl.enable = true;
   };
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome3.baobab
+    gnome3.cheese
+    gnome3.eog
+    gnome3.epiphany
+    gnome3.geary
+    gnome3.gedit
+    gnome3.gnome-calendar
+    gnome3.gnome-clocks
+    gnome3.gnome-contacts
+    gnome3.gnome-font-viewer
+    gnome3.gnome-logs
+    gnome3.gnome-maps
+    gnome3.gnome-music
+    gnome3.gnome-screenshot
+    gnome.gnome-software
+    gnome3.gnome-system-monitor
+    gnome3.gnome-weather
+    gnome-connections
+    gnome-photos
+    gnome3.simple-scan
+    gnome3.totem
+    gnome3.yelp
+  ];
 }
