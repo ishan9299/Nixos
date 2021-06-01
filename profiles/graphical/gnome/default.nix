07701a000939b5ea3a8a6534df7cd19a1dae29f1
ishan <ishan365.ia@gmail.com>
2021-06-02 01:11:18 +0530

    add gnome-tweak-tool

--- profiles/graphical/gnome/default.nix
+++ profiles/graphical/gnome/default.nix
@@ -29,29 +29,32 @@
     switcherooControl.enable = true;
   };
 
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-connections
-    gnome-photos
-    gnome-tour
-    gnome.gnome-software
-    gnome3.baobab
-    gnome3.cheese
-    gnome3.eog
-    gnome3.epiphany
-    gnome3.geary
-    gnome3.gedit
-    gnome3.gnome-calendar
-    gnome3.gnome-clocks
-    gnome3.gnome-contacts
-    gnome3.gnome-font-viewer
-    gnome3.gnome-logs
-    gnome3.gnome-maps
-    gnome3.gnome-music
-    gnome3.gnome-screenshot
-    gnome3.gnome-system-monitor
-    gnome3.gnome-weather
-    gnome3.simple-scan
-    gnome3.totem
-    gnome3.yelp
-  ];
+  environment = {
+    systemPackages = [ pkgs.gnome.gnome-tweak-tool ];
+    gnome.excludePackages = with pkgs; [
+      gnome-connections
+      gnome-photos
+      gnome-tour
+      gnome.gnome-software
+      gnome3.baobab
+      gnome3.cheese
+      gnome3.eog
+      gnome3.epiphany
+      gnome3.geary
+      gnome3.gedit
+      gnome3.gnome-calendar
+      gnome3.gnome-clocks
+      gnome3.gnome-contacts
+      gnome3.gnome-font-viewer
+      gnome3.gnome-logs
+      gnome3.gnome-maps
+      gnome3.gnome-music
+      gnome3.gnome-screenshot
+      gnome3.gnome-system-monitor
+      gnome3.gnome-weather
+      gnome3.simple-scan
+      gnome3.totem
+      gnome3.yelp
+    ];
+  };
 }
